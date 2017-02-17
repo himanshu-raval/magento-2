c30367e1e10ec8abf4eee3dbb87105bba559f51c
himanshu-raval <himanshuvraval@gmail.com>
2017-02-17 14:35:40 +0530

    Update magento-2-code.php

--- magento-2-code.php
+++ magento-2-code.php
@@ -1,4 +1,28 @@
 
-How To Enable Developer Mode Using .htaccess
+1) How To Enable Developer Mode Using .htaccess
 - The first thing you need to do is go to your .htaccess file. Uncomment
 - SetEnv MAGE_MODE developer
+
+2) Magento 2 get Product Image on list page loop
+/*
+Get Product image
+*/
+
+// echo "Product ID".$_product->getId();
+
+
+    $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+    $product = $objectManager->create('Magento\Catalog\Model\Product')->load($_product->getId());        
+$images = $product->getMediaGalleryImages();
+
+foreach($images as $child){ 
+    $img_url = $child->getUrl();
+    if(isset($img_url)){
+        $productImageUrl = $img_url;
+    }
+    break;
+}
+
+/*
+End :: Get Product image
+*/
